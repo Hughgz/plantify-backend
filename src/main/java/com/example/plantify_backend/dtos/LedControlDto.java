8ec8c334fbb0b7ec15e29e7c499c65c30f6017f5
Hughgz <minhhieu.swe@gmail.com>
2025-05-31 22:58:30 +0700

    update

--- src/main/java/com/example/plantify_backend/dtos/LedControlDto.java
+++ src/main/java/com/example/plantify_backend/dtos/LedControlDto.java
@@ -2,12 +2,16 @@ package com.example.plantify_backend.dtos;
 
 import java.time.LocalTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
 public class LedControlDto {
     private int id;
     private String ledName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime turnOnTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime turnOffTime;
 }
